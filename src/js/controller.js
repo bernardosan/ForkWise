65896184a36017d1ce2000ba78ebcb143716262b
bernardosan <bernardosantiagodesouza@gmail.com>
2023-12-21 16:48:28 +0000

    initial commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZeoOHivGA4vPeMLP37q45-y7sn42cAR8",
+  authDomain: "forkify-c8dad.firebaseapp.com",
+  projectId: "forkify-c8dad",
+  storageBucket: "forkify-c8dad.appspot.com",
+  messagingSenderId: "786511756743",
+  appId: "1:786511756743:web:15b8dd51db61fbca8eede2",
+  measurementId: "G-S665FP7DZC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  console.log(app)
+  return cityList;
+}
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
