a4d4306309b79674714157d5216deae90a8fe56e
bernardosan <bernardosantiagodesouza@gmail.com>
2023-12-22 22:55:24 +0000

    refactored code into MVC architecture

---
+++ src/js/helpers.js
@@ -0,0 +1,21 @@
+export const getJSON = async function(url){
+
+    try{
+        const response = await Promise.race[fetch(url), timeout(10)]
+        const data = await response.json();
+        console.log(data.data.recipe);
+      
+        if (!response.ok) throw new Error(`${data.message} (${res.status})`);
+        return data;
+    } catch (err){
+        throw err;
+    }
+}
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+      setTimeout(function () {
+        reject(new Error(`Request took too long! Timeout after ${s} second`));
+      }, s * 1000);
+    });
+  };
